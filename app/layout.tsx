ff95733afca7d64d2978f95a908e95b0101c97bf
Nevin Liang <nevinkliang@gmail.com>
2024-06-16 23:41:11 -0700

    fixed spacing bugs and old template code

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Nevin Liang',
+    template: '%s | Nevin Liang',
   },
   description: 'This is my portfolio.',
   openGraph: {
